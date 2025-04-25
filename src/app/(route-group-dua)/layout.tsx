0ab661c2036944b94df3f9ae86fec348ac2eb9da
virgo <virgostevanus27@gmail.com>
2025-04-25 14:43:21 +0700

    layout and route-group

---
+++ src/app/(route-group-dua)/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>INI LAYOUT UNTUK ROUTE GROUP DUA</div>
+  )
+}
---
+++ src/app/(route-group-dua)/user/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>User page inside route group dua</div>
+  )
+}
