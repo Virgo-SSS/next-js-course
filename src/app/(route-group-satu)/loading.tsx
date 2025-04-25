cc5aa2a6ad964404b6e6eb028ded5827235d558b
virgo <virgostevanus27@gmail.com>
2025-04-25 16:11:17 +0700

    loading

---
+++ src/app/(route-group-satu)/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <h1 className='text-2xl font-bold'>Test Loading... </h1>
+        </div>
+    )
+  }
