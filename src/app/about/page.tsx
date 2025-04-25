e0dc5a12c7b0115b7fb97b0a789ee087a9f2d502
virgo <virgostevanus27@gmail.com>
2025-04-25 14:00:21 +0700

    client side - server side rendering

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    console.log("console log from about.tsx - server side");
+    return (
+        <>
+        <h1>About</h1>
+        <p>This is the about page.</p>
+        </>
+    );
+}
---
+++ src/app/components/TestClientComponent.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export default function TestClientComponent() {
+    console.log("TestClientComponent rendered");
+    return <h1>Test Client Component</h1>;
+}
